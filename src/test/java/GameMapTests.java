7fcebeaa6a9c55559f4253e19e751556f65cd0f4
Tania <azartania1@gmail.com>
2017-12-26 13:14:15 +0300

    - добавили тесты для игрового поля(GameMap)

---
+++ src/test/java/GameMapTests.java
@@ -0,0 +1,115 @@
+import by.itclass.game.core.Cell;
+import by.itclass.game.core.GameMap;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GameMapTests {
+
+    @Test
+    public void testMapCreation(){
+        GameMap map = new GameMap(10,10);
+        Assert.assertNotNull(map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMapCreationIllegalWidth(){
+        GameMap map = new GameMap(-10,10);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMapCreationIllegalHeight(){
+        GameMap map = new GameMap(10,-10);
+    }
+
+    @Test
+    public void testMapWidthGetter(){
+        GameMap map = new GameMap(20,30);
+        int actual = map.getWidth();
+        int expected = 20;
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testMapHeightGetter(){
+        GameMap map = new GameMap(20,30);
+        int actual = map.getHeight();
+        int expected = 30;
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testRightCellGetter(){
+        GameMap map = new GameMap(5,5);
+        Cell cell = map.getCell(3,3);
+        Assert.assertNotNull(cell);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCellGetterJNegative(){
+        GameMap map = new GameMap(5,5);
+        Cell cell = map.getCell(3,-3);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCellGetterINegative(){
+        GameMap map = new GameMap(5,5);
+        Cell cell = map.getCell(-3,3);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCellGetterJTooBig(){
+        GameMap map = new GameMap(5,5);
+        Cell cell = map.getCell(3,7);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCellGetterITooBig(){
+        GameMap map = new GameMap(5,5);
+        Cell cell = map.getCell(9,3);
+    }
+
+    @Test
+    public void testRightCellSetter(){
+        GameMap map = new GameMap(7,7);
+        Cell expected = new Cell(19);
+
+        map.setCell(1,5,expected);//тестируем данный метод
+        Cell actual = map.getCell(1,5);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCellSetterJNegative(){
+        GameMap map = new GameMap(5,5);
+        Cell cell = new Cell(2);
+        map.setCell(3,-3,cell);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCellSetterINegative(){
+        GameMap map = new GameMap(5,5);
+        Cell cell = new Cell(2);
+        map.setCell(-3,3,cell);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCellSetterJTooBig(){
+        GameMap map = new GameMap(5,5);
+        Cell cell = new Cell(2);
+        map.setCell(3,8,cell);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCellSetterITooBig(){
+        GameMap map = new GameMap(5,5);
+        Cell cell = new Cell(2);
+        map.setCell(9,3,cell);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCellSetterNullCell(){
+        GameMap map = new GameMap(5,5);
+        Cell cell = null;
+        map.setCell(3,3,cell);
+    }
+}
