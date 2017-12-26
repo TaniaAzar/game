97c6aa1813a4cb436b8d9886d4152b793bb0d0b2
Tania <azartania1@gmail.com>
2017-12-26 11:21:15 +0300

    - добавили класс ячейки на игровом поле

---
+++ src/main/java/by/itclass/game/core/Cell.java
@@ -0,0 +1,18 @@
+package by.itclass.game.core;
+
+//Клетка на игровом поле
+public class Cell {
+
+    private int type;
+
+    public Cell(int type){
+        if (type < 0){
+            throw new IllegalArgumentException("Неверный тип клетки");
+        }
+        this.type = type;
+    }
+
+    public int getType(){
+        return type;
+    }
+}
